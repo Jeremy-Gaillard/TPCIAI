ecb58a73d135dcd1d0d2949465bac4dfc4aa594f
Jeremy Gaillard <jeremy.c.gaillard@gmail.com>
2012-11-10 17:53:10 +0100

    Création des threads et destruction des IPC

--- Programme/Linux/mere.c
+++ Programme/Linux/mere.c
@@ -7,6 +7,10 @@
 #include <sys/shm.h>
 #include <pthread.h>
 
+#include <unistd.h> /*temporaire (sleep)*/
+
+#include <stdio.h>
+
 
 const char BALERR = 'A';
 const char BALDIS = 'B';
@@ -30,6 +34,9 @@ int main(int argc, char** argv)
 		sem_erreur_carton, sem_erreur_palette, sem_AU;	/*id des semaphores*/
 	pthread_mutex_t mutex_entrepot, mutex_lot; /*mutex*/
 	pid_t shm_statut, shm_lot, shm_entrepot; /*id des mémoires partagées*/
+	
+	pthread_t t_carton, t_palette, t_cariste, t_erreur, t_log_disque, 
+		t_log_windows, t_commande_windows;
 	
 	/*Initialisation*/
 	
@@ -66,9 +73,43 @@ int main(int argc, char** argv)
 	shm_entrepot = shmget( ftok( argv[0], SHMENT ), sizeof(struct entrepot), IPC_CREAT | MODERW);
 	shm_lot = shmget( ftok( argv[0], SHMLOT ), 2*sizeof(int), IPC_CREAT | MODERW);
 	
+	/*Threads*/
+	/*pthread_create( &t_carton, NULL, &carton, ? );
+	pthread_create( &t_log_disque, NULL, &log_disque, ? );
+	pthread_create( &t_log_windows, NULL, &log_windows, ? );
+	pthread_create( &t_palette, NULL, &palette, ? );
+	pthread_create( &t_cariste, NULL, &cariste, ? );
+	pthread_create( &t_erreur, NULL, &erreur, ? );
+	pthread_create( &t_commande_windows, NULL, &commande_windows, ? );*/
+	
 	/*Moteur*/
+	sleep( 5 );
+	/*pthread_join( t_commande_windows, NULL );*/
 	
 	/*Destruction*/
+	
+	/*Mémoire partagées*/
+	shmctl( shm_lot, IPC_RMID, NULL);
+	shmctl( shm_entrepot, IPC_RMID, NULL);
+	shmctl( shm_statut, IPC_RMID, NULL);
+	
+	/*Mutex*/
+	pthread_mutex_destroy( &mutex_entrepot );
+	pthread_mutex_destroy( &mutex_lot );
+	
+	/*Sémaphores*/
+	semctl( sem_AU, 0, IPC_RMID, NULL );
+	semctl( sem_erreur_palette, 0, IPC_RMID, 0 );
+	semctl( sem_erreur_carton, 0, IPC_RMID, 0 );
+	semctl( sem_palette, 0, IPC_RMID, 0 );
+	semctl( sem_carton, 0, IPC_RMID, 0 );
+	semctl( sem_piece, 0, IPC_RMID, 0 );
+	semctl( sem_clapet, 0, IPC_RMID, 0 );
+	
+	/*Boîtes aux lettres*/
+	msgctl( bal_log_windows, IPC_RMID, NULL );
+	msgctl( bal_log_disque, IPC_RMID, NULL );
+	msgctl( bal_erreur, IPC_RMID, NULL );
 	
 	return 0;
 }
