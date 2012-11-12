6198269f6742d374c9f36e45e7722b3338193dc2
Fhyren <fhyren@fhyren-G74Sx.(none)>
2012-11-12 15:29:03 +0100

    new files (log_windows) + some monir changes

---
+++ Programme/Linux/Simulation/simulation.h
@@ -0,0 +1,7 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+void simulation();
+
+#endif
+
---
+++ Programme/Linux/log_windows.h
@@ -0,0 +1,10 @@
+#ifndef LOG_WINDOWS_H
+#define LOG_WINDOWS_H
+
+#include "config.h"
+
+void envoyer(log_t message);
+void log_windows();
+
+#endif
+
