34b1a216152a8e5218064f50636ebc105b410219
peniblec <peniblec@peniblec-laptop.(none)>
2012-11-12 08:18:05 +0100

    changé la structure de la mémoire partagée de statut (1 char -> STATUT_SIZE int)

--- Programme/Linux/config.h
+++ Programme/Linux/config.h
@@ -6,9 +6,17 @@
 #define CARTON_PLEIN 10
 #define MAX_REBUS 10
 
+#define STATUT_SIZE 6
+#define ST_PRESENCE_CARTON 0
+#define ST_PRESENCE_PALETTE 1
+#define ST_IMPRIMANTE 2
+#define ST_PIECE_OK 3
+#define ST_FILM_OK 4
+#define ST_CLAPET_OUVERT 5
+
 typedef char log_t[128];
 typedef int lot_t[2];
-typedef char statut_t;
+typedef int statut_t[STATUT_SIZE];
 
 typedef struct erreur {
 	int id;
