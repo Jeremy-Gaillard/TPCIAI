1e2709e292049872bdcec7e7a5d91685a26a811e
peniblec <peniblec@peniblec-laptop.(none)>
2012-11-12 09:59:27 +0100

    ajouté les déclarations des threads de production dans prod_utils.h

--- Programme/Linux/prod_utils.h
+++ Programme/Linux/prod_utils.h
@@ -6,6 +6,15 @@
 #include <mqueue.h>
 #include "config.h"
 
+
+int carton( mqd_t bal_erreur, mdq_t bal_log_disque, mdq_t bal_log_windows,
+            sem_t sem_piece, sem_t sem_carton, sem_t sem_erreur_carton,
+            statut_t shm_statut );
+
+int palette();
+
+int cariste();
+
 void gerer_erreur( int erreur_id, mqd_t bal_erreur );
 
 #endif
