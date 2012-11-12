d2392fcfa133fb5661dbb7bc6255be2037286d54
mcaffart <mcaffart@gmail.com>
2012-11-12 00:55:38 -0800

    Ajout de l'interface windows

---
+++ Programme/Windows/Interface_windows_CIAI/src/interface_windows_ciai/Interface_windows_CIAI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interface_windows_ciai;
+
+/**
+ *
+ * @author michael
+ */
+public class Interface_windows_CIAI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
