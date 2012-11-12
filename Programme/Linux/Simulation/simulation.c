3e512015789a3765fa7ae3c3601b32731eccbba3
Fhyren <fhyren@fhyren-G74Sx.(none)>
2012-11-12 16:28:40 +0100

    bug fixed on switch + shm_statut management

--- Programme/Linux/Simulation/simulation.c
+++ Programme/Linux/Simulation/simulation.c
@@ -7,12 +7,13 @@
 #include <sys/ipc.h>
 #include <mqueue.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 #include <errno.h>
 
 #include "simulation.h"
 
-void simulation()
+void simulation(statut_t* shm_statut)
 {
 	int bal_log_disque = mq_open( BALDIS, O_WRONLY );
 	char commande[20];
@@ -21,7 +22,7 @@ void simulation()
 		scanf("%s", commande);
 		if( !strcmp(commande, "logd") )
 		{
-			mq_send(bal_log_disque, "Hello", sizeof(log_t), 1);
+			mq_send(bal_log_disque, "E 2", sizeof(log_t), 1);
 		}
 		if( !strcmp(commande, "logdf") )
 		{
@@ -29,63 +30,64 @@ void simulation()
 		}
 		else if( !strcmp(commande, "presence_carton") )
 		{
-			shm_statut[ ST_PRESENCE_CARTON ] = 1;
+			*shm_statut[ ST_PRESENCE_CARTON ] = 1;
 		}
 		else if( !strcmp(commande, "absence_carton") )
 		{
-			shm_statut[ ST_PRESENCE_CARTON ] = 0;
+			*shm_statut[ ST_PRESENCE_CARTON ] = 0;
 		}
 		else if( !strcmp(commande, "presence_palette") )
 		{
-			shm_statut[ ST_PRESENCE_PALETTE ] = 1;
+			*shm_statut[ ST_PRESENCE_PALETTE ] = 1;
 		}
 		else if( !strcmp(commande, "absence_palette") )
 		{
-			shm_statut[ ST_PRESENCE_PALETTE ] = 0;
+			*shm_statut[ ST_PRESENCE_PALETTE ] = 0;
 		}
 		else if( !strcmp(commande, "imprimante_OK") )
 		{
-			shm_statut[ ST_IMPRIMANTE ] = 1;
+			*shm_statut[ ST_IMPRIMANTE ] = 1;
 		}
 		else if( !strcmp(commande, "err_imprimante_KO") )
 		{
-			shm_statut[ ST_IMPRIMANTE ] = 0;
+			*shm_statut[ ST_IMPRIMANTE ] = 0;
 		}
 		else if( !strcmp(commande, "presence_carton") )
 		{
-			shm_statut[ ST_PRESENCE_CARTON ] = 1;
+			*shm_statut[ ST_PRESENCE_CARTON ] = 1;
 		}
 		else if( !strcmp(commande, "absence_carton") )
 		{
-			shm_statut[ ST_PRESENCE_CARTON ] = 0;
+			*shm_statut[ ST_PRESENCE_CARTON ] = 0;
 		}
 		else if( !strcmp(commande, "piece_OK") )
 		{
-			shm_statut[ ST_PIECE ] = 1;
+			*shm_statut[ ST_PIECE ] = 1;
 		}
 		else if( !strcmp(commande, "piece_KO") )
 		{
-			shm_statut[ ST_PIECE ] = 0;
+			*shm_statut[ ST_PIECE ] = 0;
 		}
 		else if( !strcmp(commande, "film_OK") )
 		{
-			shm_statut[ ST_FILM ] = 1;
+			*shm_statut[ ST_FILM ] = 1;
 		}
 		else if( !strcmp(commande, "film_KO") )
 		{
-			shm_statut[ ST_FILM ] = 0;
+			*shm_statut[ ST_FILM ] = 0;
 		}
 		else if( !strcmp(commande, "clapet_ouvert") )
 		{
-			shm_statut[ ST_CLAPET_OUVERT ] = 1;
+			*shm_statut[ ST_CLAPET_OUVERT ] = 1;
 		}
 		else if( !strcmp(commande, "clapet_ferme") )
 		{
-			shm_statut[ ST_CLAPET_OUVERT ] = 0;
+			*shm_statut[ ST_CLAPET_OUVERT ] = 0;
 		}
 		if( !strcmp(commande, "q") )
 		{
 			break;
 		}
 	}
+	pthread_exit(0);
 }
--- Programme/Linux/log_disque.c
+++ Programme/Linux/log_disque.c
@@ -22,67 +22,70 @@ void log_disque()
 	message[0] = '\0';
 	
 	fichier_log = fopen(NOM_LOG, "w");
-	fprintf(fichier_log, "Début de session");
+	fprintf(fichier_log, "Début de session\n");
 	
 	while( strcmp(message, TRAME_FIN) )
 	{
 		mq_receive(bal_log_disque, (void*) &message, sizeof(log_t), NULL);
 		/*Analyser le message ici*/
 		log_t log = "";
-		switch (message[0])
-		{
-			case 'L':
-				switch (message[2])
-				{
-					case 'C':
-						/*TODO*/
-						break;
-					case 'P':
-						/*TODO*/
-						break;
-					default:
-						log[0] = '\0';
-						break;
-				}
-				break;
-			case 'E':
-				strcat( log, "Erreur : ");
-				switch (message[2])
-				{
-					case 'ERR_AU':
-						strcat( log, "arrêt d'urgence");
-						break;
-					case 'ERR_TROP_DE_REBUS':
-						strcat( log, "trop de rebus");
-						break;
-					case 'ERR_PAS_DE_CARTON':
-						strcat( log, "plus de carton");
-						break;
-					case 'ERR_IMPRIMANTE_KO':
-						strcat( log, "imprimante hors service");
-						break;
-					case 'ERR_FILE_D_ATTENTE':
-						strcat( log, "trop de carton en attente d'être empaletté");
-						break;
-					case 'ERR_PAS_DE_PALETTE':
-						strcat( log, "plus de palette");
-						break;
-					case 'ERR_FILM_KO':
-						strcat( log, "dysfonction du film palette");
-						break;
-					default:
-						log[0] = '\0';
-						break;
-				}
-			default:
-				log[0] = '\0';
-				break;
-		}
 		
-		if( strcmp(message, TRAME_FIN) )
-			fprintf(fichier_log, "Fin de session");
-		if( log[0] != '\0' )
-			fprintf(fichier_log, "log_disque : %s\n", message);
+		if( !strcmp(message, TRAME_FIN) )
+			fprintf(fichier_log, "Fin de session\n");
+		else
+		{
+			switch (message[0])
+			{
+				case 'L':
+					switch (message[2])
+					{
+						case 'C':
+							/*TODO*/
+							break;
+						case 'P':
+							/*TODO*/
+							break;
+						default:
+							log[0] = '\0';
+							break;
+					}
+					break;
+				case 'E':
+					strcat( log, "Erreur : ");
+					switch (message[2] - '0')
+					{
+						case ERR_AU:
+							strcat( log, "arrêt d'urgence\n");
+							break;
+						case ERR_TROP_DE_REBUS:
+							strcat( log, "trop de rebus\n");
+							break;
+						case ERR_PAS_DE_CARTON:
+							strcat( log, "plus de carton\n");
+							break;
+						case ERR_IMPRIMANTE_KO:
+							strcat( log, "imprimante hors service\n");
+							break;
+						case ERR_FILE_D_ATTENTE:
+							strcat( log, "trop de carton en attente d'être empaletté\n");
+							break;
+						case ERR_PAS_DE_PALETTE:
+							strcat( log, "plus de palette\n");
+							break;
+						case ERR_FILM_KO:
+							strcat( log, "dysfonction du film palette\n");
+							break;
+						default:
+							log[0] = '\0';
+							break;
+					}
+				default:
+					log[0] = '\0';
+					break;
+			}	
+			if( log[0] != '\0' )
+				fprintf(fichier_log, "log_disque : %s\n", log);
+		}
 	}
 	
 	fclose(fichier_log);
