b76a24cb5967d8ad25f0d4fe96aaab84d8520aa7
peniblec <peniblec@peniblec-laptop.(none)>
2012-11-12 09:44:02 +0100

    changements sur la tâche carton

--- Programme/Linux/carton.c
+++ Programme/Linux/carton.c
@@ -1,58 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <semaphore.h>
 #include "config.h"
+#include "prod_utils.h"
 
-int carton(){
+int carton( mqd_t bal_erreur, mdq_t bal_log_disque, mdq_t bal_log_windows,
+            sem_t sem_piece, sem_t sem_carton, sem_t sem_erreur_carton,
+            statut_t shm_statut ){
 	int nb_piece = 0;
 	int nb_carton = 0;
 	int nb_rebus =0;
 	int place_file_attente;
-	time_t rawtime;
-	struct tm * timeinfo;
 
 	for( ; ; ){
 		/* attente piece */
 		sem_wait( &sem_piece ); 
 
-		if ( nb_piece == 0 && shm_statut [ precence_carton ] != '1' ){
+		if ( nb_piece == 0 && shm_statut[ST_PRESENCE_CARTON] != 1 ){
 		/*si premiere piece et absence carton
-		 envoie d'un message d'erreur avec hhmmss et type erreur
+		 envoi d'un message d'erreur avec hhmmss et type erreur
 		 puis attente sur semaphore de reprise d'erreur*/
 		 		
-			struct erreur absence_carton;
-  			time ( &rawtime );
-  			timeinfo = localtime ( &rawtime );
-			strftime ( absence_carton.heure, 7, "%H%M%S", timeinfo );
-			absence_carton.id=2;
-			
-			mq_send( bal_erreur, absence_carton, sizeof( absence_carton ),
-				1 );
-			
+			gerer_erreur_carton(ERR_PAS_DE_CARTON, bal_erreur, sem_erreur_carton);
 			sem_wait( &sem_erreur_carton );
 		}
 		/*end of absence carton*/
 		
-		if ( shm_statut[ piece_OK ] == '1' ){
+		if ( shm_statut[ ST_PIECE_OK ] == 1 ){
 			nb_piece += 1;
 			if ( nb_piece == CARTON_PLEIN ){
-				if ( shm_statut[ imprimante ] != '1' ){
+				if ( shm_statut[ST_IMPRIMANTE] != 1 ){
 				/*si carton plein et imprimante HS
 				 envoie d'un message d'erreur avec hhmmss et type erreur
 				 puis attente sur semaphore de reprise d'erreur*/
-				 
-					struct erreur imprimante_KO;
-					time ( &rawtime );
-					timeinfo = localtime ( &rawtime );
-					strftime ( imprimante_KO.heure, 7,
-						"%H%M%S", timeinfo );
-					imprimante_KO.id=3;
 
-					mq_send( bal_erreur, imprimante_KO,
-					 sizeof( imprimante_KO ), 1 );
-					 
-					sem_wait( &sem_erreur_carton ); 
+					gerer_erreur_carton(ERR_IMPRIMANTE_KO, bal_erreur, sem_erreur_carton);
+					sem_wait( &sem_erreur_carton );
 				}
 				/*end of if imprimante HS*/
 				
@@ -62,18 +45,8 @@ int carton(){
 				 envoie d'un message d'erreur avec hhmmss et type erreur
 				 puis attente sur semaphore de reprise d'erreur*/
 				 
-					struct erreur file_pleine;
-		  			time ( &rawtime );
-		  			timeinfo = localtime ( &rawtime );
-					strftime ( file_pleine.heure, 7,
-						"%H%M%S", timeinfo );
-					imprimante_KO.id=3;
-
-					mq_send( bal_erreur, file_pleine,
-					 sizeof( file_pleine ), 1 );
-					 
-					 sem_wait( &sem_erreur_carton );
-					 
+					gerer_erreur_carton(ERR_FILE_D_ATTENTE, bal_erreur, sem_erreur_carton);
+					sem_wait( &sem_erreur_carton );
 				}
 				/*end of if file attente pleine*/
 				
@@ -84,6 +57,7 @@ int carton(){
 				l'info carton plein*/
 				nb_piece = 0;
 				nb_rebus = 0;
+				nb_carton = (nb_carton==PALETTE_PLEINE) ? 0 : nb_carton;
 			}
 			/*end of if carton plein*/
 		
@@ -97,18 +71,10 @@ int carton(){
 			 envoie d'un message d'erreur avec hhmmss et type erreur
 			 puis attente sur semaphore de reprise d'erreur
 			 puis on jette le carton en cours*/
-			 		
-				struct erreur trop_rebus;
-		  			time ( &rawtime );
-		  			timeinfo = localtime ( &rawtime );
-					strftime ( trop_rebus.heure, 7,
-						"%H%M%S", timeinfo );
-				trop_rebus.id=2;
-
-				mq_send( bal_erreur, trop_rebus, 
-					sizeof( trop_rebus ) , 1 );
-			
+			 	
+				gerer_erreur_carton(ERR_TROP_DE_REBUS, bal_erreur, sem_erreur_carton);
 				sem_wait( &sem_erreur_carton );
+
 				nb_piece = 0;
 				nb_rebus = 0;
 			}
