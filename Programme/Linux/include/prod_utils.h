bff799cafa0819383df058e389ed056349dab4dd
Peniblec <peniblec@crunchbang-peniblec.(none)>
2012-11-13 23:08:30 +0100

    défini la structure à envoyer à carton pour son initialisation

--- Programme/Linux/include/prod_utils.h
+++ Programme/Linux/include/prod_utils.h
@@ -7,15 +7,28 @@
 #include <semaphore.h>
 #include "config.h"
 
+typedef struct arg_carton
+{
+	statut_t* shm_statut;
 
-int carton( mqd_t bal_erreur, mqd_t bal_log_disque, mqd_t bal_log_windows,
-            sem_t sem_piece, sem_t sem_carton, sem_t sem_erreur_carton,
-            statut_t shm_statut );
+	sem_t* bal_erreur;
+	sem_t* bal_log_win;
+	sem_t* bal_log_disque;
+
+  sem_t* sem_piece;
+  sem_t* sem_carton;
+  sem_t* sem_erreur_carton;
+} arg_carton_t;
+
+
+
+
+int carton( arg_carton_t args );
 
 int palette();
 
 int cariste();
 
-void gerer_erreur( int erreur_id, mqd_t bal_erreur );
+void gerer_erreur( int erreur_id );
 
 #endif
