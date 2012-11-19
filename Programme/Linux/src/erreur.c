57029a2ca6bda37169d9d8ad6d3bd86f349136cb
Jeremy-Gaillard <jeremy.c.gaillard@gmail.com>
2012-11-19 16:26:40 +0100

    fix erreur qi envoie trame_fin a diqsue et windows

--- Programme/Linux/src/erreur.c
+++ Programme/Linux/src/erreur.c
@@ -26,15 +26,18 @@ void erreur(struct arg_erreur *ipc)
 	do
 	{
 		mq_receive(bal_log_erreur, message, sizeof(erreur_t), NULL);
-		(*shm_statut)[ST_CLAPET_OUVERT] = 0;
-		log_t log;
-		sprintf( log, "E %s", message );
-		pthread_mutex_lock ( mutex_windows );
-		mq_send(bal_log_windows, log, sizeof(log_t), BAL_PRIO_ELSE);
-		pthread_mutex_unlock ( mutex_windows );
-		pthread_mutex_lock ( mutex_disque );
-		mq_send(bal_log_disque, log, sizeof(log_t), BAL_PRIO_ELSE);
-		pthread_mutex_unlock ( mutex_disque );
+		if( message != TRAME_FIN )
+		{
+			(*shm_statut)[ST_CLAPET_OUVERT] = 0;
+			log_t log;
+			sprintf( log, "E %s", message );
+			pthread_mutex_lock ( mutex_windows );
+			mq_send(bal_log_windows, log, sizeof(log_t), BAL_PRIO_ELSE);
+			pthread_mutex_unlock ( mutex_windows );
+			pthread_mutex_lock ( mutex_disque );
+			mq_send(bal_log_disque, log, sizeof(log_t), BAL_PRIO_ELSE);
+			pthread_mutex_unlock ( mutex_disque );
+		}
 	}
 	while( strcmp(message, TRAME_FIN) );
 	printf("Fin thread erreur\n");
