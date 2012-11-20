6e20b1d5eb4dbfb31f3fee3fd60545a8f0246d8b
coralie-saysset <coralie.saysset@insa-lyon.fr>
2012-11-20 14:46:17 +0100

    comment prod_utils.c

--- Programme/Linux/src/prod_utils.c
+++ Programme/Linux/src/prod_utils.c
@@ -11,7 +11,7 @@
 
 void gerer_erreur( int erreur_id, pthread_mutex_t* mutex_erreur )
 {
-	
+	/*Recuperation de l'heure au format HHMMSS*/
 	char heure[7];
 	time_t rawtime;
 	struct tm * timeinfo;
@@ -19,9 +19,13 @@ void gerer_erreur( int erreur_id, pthread_mutex_t* mutex_erreur )
 	time ( &rawtime );
 	timeinfo = localtime ( &rawtime );
 	strftime ( heure, 7, "%H%M%S", timeinfo );
+	
+	/*creation de message erreur : type_erreur hhmmss*/
 	erreur_t message_erreur;
 	sprintf(message_erreur, "%d %s", erreur_id, heure);
 
+	/*envoi du message a la bal d'erreur
+	celle ci est protegee par un mutex: mutrex_erreur*/
 	mqd_t bal_erreur = mq_open(BALERR, O_WRONLY);
 	pthread_mutex_lock( mutex_erreur );
 	mq_send( bal_erreur, message_erreur, sizeof( erreur_t ),
